2aaa0c2011d35c938d9a5aef947e8b25a18a227c
test1234 <esm@DESKTOP-7F452E7>
2024-09-18 17:26:24 +0900

    signUp(회원가입) API 명세서 수정

--- routes/publicRouter.js
+++ routes/publicRouter.js
@@ -42,38 +42,48 @@ router.post("/signUp", signUpProcess);
  * /api/public/signUp/checkNickName:
  *   post:
  *       summary: 닉네임 중복확인 요청
+ *       description: 회원가입 과정에서 , NickName이 중복되었는지를 확인함
  *       tags:
  *          - signUp
  *       requestBody:
- *           description: 회원가입 과정에서 , NickName이 중복되었는지를 확인함
+ *           description: 중복을 확인하기 위한 nickName
  *           content:
- *               application/json:
- *                   schema:
- *                       $ref: "#components/schemas/Response_DuplicateCheckNickName"
+ *              application/json:
+ *                  schema:
+ *                      $ref: "#components/schemas/Request_NickName"
  *
  *       responses:
  *           200:
- *               description: 중복된 NickName이 존재하는지 확인함
+ *              description: 중복된 NickName이 존재할 시 false , 없을 시 true
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#components/schemas/Response_DuplicateCheckNickName"
  */
 router.post("/signUp/checkNickName", checkNickname);
 
 /**
  * @swagger
  * /api/public/signUp/checkLogInId:
- *   post:
- *       summary: 로그인 ID 중복확인 요청
- *       tags:
- *           - signUp
- *       requestBody:
- *           description: 회원가입 과정에서 , LogInId가 중복되었는지를 확인함
- *           content:
- *               application/json:
- *                   schema:
- *                       $ref: "#components/schemas/Response_DuplicateCheckLogInId"
+ *    post:
+ *        summary: 로그인 ID 중복확인 요청
+ *        description: 회원가입 과정에서 , LogInId가 중복되었는지를 확인함
+ *        tags:
+ *            - signUp
+ *        requestBody:
+ *            description: 중복을 확인하기 위한 userLogInId
+ *            content:
+ *                application/json:
+ *                    schema:
+ *                        $ref: "#components/schemas/Request_LogInId"
  *
- *       responses:
- *           200:
- *               description: 중복된 LogInId가 존재하는지 확인함
+ *        responses:
+ *            200:
+ *                description: 중복된 LogInId가 존재할 시 false , 없을 시 true
+ *                content:
+ *                  application/json:
+ *                      schema:
+ *                          $ref: "#components/schemas/Response_DuplicateCheckLogInId"
  */
 router.post("/signUp/checkUserLogInId", checkUserLogInId);
 
